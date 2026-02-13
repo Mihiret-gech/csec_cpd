6d161265bc285e44565ba228eeb273880ac6a074
Mihiret-gech <mihiretgetahun2112@gmail.com>
2026-02-13 10:00:43 -0800

    Implement game logic for Anton and Danik

--- Anton and Danik.py
+++ Anton and Danik.py
@@ -1,1 +1,18 @@
-
+n = int(input())
+s = input()
+ 
+A = 0
+D = 0
+ 
+for letter in s:
+    if letter == "A":
+        A += 1
+    else:
+        D += 1
+ 
+if A > D:
+    print("Anton")
+elif A < D:
+    print("Danik")
+else:
+    print("Friendship")
